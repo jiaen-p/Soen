23d7edf8b962c4e86f74062f87ad1f9440136997
Cris M <crismedinagomez@gmail.com>
2020-07-15 22:44:59 +0200

    formularios con funcionalidad

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user;
+
+  constructor() 
+  {
+    this.user = new User();
+  }
+
+
+  onSubmit(form)
+  {
+    console.log(form.value);
+  }
 
   ngOnInit(): void {
   }
+
 
 }
--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from "../../models/user";
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public profile: boolean;
+  public user: User;
+
+  constructor() {
+    this.profile = false;
+    this.user = new User();
+  }
+
+  profile_select(){
+    this.profile = true;
+  }
+
+  onSubmit(form)
+  {
+    console.log(form.value);
+  }
 
   ngOnInit(): void {
   }
