4515c6c3a9b0129e763c037d4f774c8b066b6fcb
Manuel <manesga-th@hotmail.com>
2020-07-17 22:20:39 +0200

    forulario registro empresa

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegistroEmpresaComponent } from './pages/registro-empresa/registro-empresa.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-    DashboardComponent
+    DashboardComponent,
+    RegistroEmpresaComponent
 
   ],
   imports: [
---
+++ client/src/app/models/empresa.ts
@@ -0,0 +1,18 @@
+import { Sectores } from './sectores.enum'
+export class Empresa {
+    public nombre: string
+    public direccion: string
+    public nif: string
+    public telefono: number
+    public fax: number
+    public email: string
+    public web: string
+    public descripcion: string
+    public img_url: string
+    public id: number
+    public sector: Sectores
+   
+}
+
+
+
