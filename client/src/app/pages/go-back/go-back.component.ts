269004aff91101d7800f386aae4d8db52126bf12
Jiaen Pan <jiaen.m.pan@gmail.com>
2020-08-12 11:17:37 +0200

    añadido boton volver en paginas de editar proyecto y empleo, cambiado el titulo del documento

---
+++ client/src/app/pages/go-back/go-back.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-go-back',
+  templateUrl: './go-back.component.html',
+  styleUrls: ['./go-back.component.css']
+})
+export class GoBackComponent implements OnInit {
+
+  constructor(public _location:Location) { }
+
+  ngOnInit(): void {
+  }
+
+}
